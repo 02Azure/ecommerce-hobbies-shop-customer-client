b9d2c76fc73d6fe603ede8d8bee81ce20e654152
02Azure <agusdomas1st@gmail.com>
2021-04-17 17:21:39 +0700

    deployed

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link :to="{ name: 'ProductDetail', params: { id: product.id } }" class="image-container d-flex align-items-center">
-      <img class="product-image-card" :src="product.image_url" alt="product-image">
+      <img class="product-image-card" :src="product.image_url" onerror="this.src = 'https://www.arraymedical.com/wp-content/uploads/2018/12/product-image-placeholder.jpg'" alt="product-image">
     </router-link>
       <button v-if="isLoggedIn" @click="addUserCarts({ ProductId: product.id, name: product.name, quantity: 1 })" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Add to cart</button>
       <router-link v-else :to="{ name: 'Login' }"><button @click="askLogin()" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Add to cart</button></router-link>
